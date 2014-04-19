7141a083ff22e8f83a488ffb194eb98dd0d85e6c
Sid Grabosky <sid@grabosky.com>
2014-04-19 18:12:15 -0400

    Moved the rest of the home page content off to content blocks

---
+++ includes/sections/content/home.php
@@ -0,0 +1,23 @@
+<div class="container" style="text-align: center;">
+	<section id="sections">
+		<div class="section">
+			This is some text and maybe a small graphic that describes a site section. Click me!
+		</div>
+		<div class="divider"><div>&nbsp;</div></div>
+		<div class="section">
+			This is some text and maybe a small graphic that describes a site section. Click me!
+		</div>
+		<div class="divider"><div>&nbsp;</div></div>
+		<div class="section">
+			This is some text and maybe a small graphic that describes a site section. Click me!
+		</div>
+	</section>
+	<div id="bigSearch">
+		<h2 style="font-size: inherit;">Search for a Linux Command:</h2>
+		<form name="siteSearch" id="siteSearch">
+			<input type="text" name="searchInput" id="searchInput/">
+			<button>Search</button>
+		</form>
+		or <a href="reference.html">view entire command reference.</a>
+	</div>
+</div>
--- index.php
+++ index.php
@@ -21,34 +21,10 @@
 			<h1><span class="logoLinux">Linux</span><span class="logoTuts">Tuts</span></h1>
 		</div>
 	</header>
-	<div class="container" style="text-align: center;">
-		<section id="sections">
-			<div class="section">
-				This is some text and maybe a small graphic that describes a site section. Click me!
-			</div>
-			<div class="divider"><div>&nbsp;</div></div>
-			<div class="section">
-				This is some text and maybe a small graphic that describes a site section. Click me!
-			</div>
-			<div class="divider"><div>&nbsp;</div></div>
-			<div class="section">
-				This is some text and maybe a small graphic that describes a site section. Click me!
-			</div>
-		</section>
-		<div id="bigSearch">
-					<h2 style="font-size: inherit;">Search for a Linux Command:</h2>
-					<form name="siteSearch" id="siteSearch">
-						<input type="text" name="searchInput" id="searchInput/">
-						<button>Search</button>
-					</form>
-					or <a href="reference.html">view entire command reference.</a>
-				</div>
-		</div>
+	<?php echo $page->getSection('content'); ?>
 	<footer>
 		<div id="footerContainer">
-			<a href="#">Home</a> | 
-			<a href="#">About</a> | 
-			<a href="#">Contact</a>
+			<?php echo $page->getSection('footer'); ?>
 		</div>
 	</footer>
 </body>
