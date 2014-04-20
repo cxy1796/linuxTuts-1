703a9288e758acb03a6c2fa2a61a8c867bb6f2be
Sid Grabosky <sid@grabosky.com>
2014-04-19 23:22:27 -0400

    Added simple Command Reference index page

---
+++ includes/sections/content/reference.php
@@ -0,0 +1,53 @@
+<div class="container">
+	<h2>Command Reference</h2>
+	<table id="commands">
+		<thead>
+	        <tr>
+	            <th>Command Name</th>
+	            <th>Description</th>
+	        </tr>
+	    </thead>
+	    <tbody>
+			<tr>
+				<td>Command Name</td>
+				<td>Info</td>
+			</tr>
+			<tr>
+				<td>Command Name</td>
+				<td>Info</td>
+			</tr>
+			<tr>
+				<td>Command Name</td>
+				<td>Info</td>
+			</tr>
+			<tr>
+				<td>Command Name</td>
+				<td>Info</td>
+			</tr>
+			<tr>
+				<td>Command Name</td>
+				<td>Info</td>
+			</tr>
+			<tr>
+				<td>Command Name</td>
+				<td>Info</td>
+			</tr>
+			<tr>
+				<td>Command Name</td>
+				<td>Info</td>
+			</tr>
+			<tr>
+				<td>Command Name</td>
+				<td>Info</td>
+			</tr>
+			<tr>
+				<td>Command Name</td>
+				<td>Info</td>
+			</tr>
+			<tr>
+				<td>Command Name</td>
+				<td>Info</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
