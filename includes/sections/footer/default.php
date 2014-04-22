e96b0687cd6e5eed8e2984023fbf7084f1a7a2c5
Sid Grabosky <sid@grabosky.com>
2014-04-21 23:03:05 -0400

    Removed redundant slash in footer URLs

--- includes/sections/footer/default.php
+++ includes/sections/footer/default.php
@@ -1,3 +1,3 @@
-<a href="<?php echo $this->getRootURL(); ?>/">Home</a> | 
-<a href="<?php echo $this->getRootURL(); ?>/about/">About</a> | 
-<a href="<?php echo $this->getRootURL(); ?>/contact/">Contact</a>
+<a href="<?php echo $this->getRootURL(); ?>">Home</a> | 
+<a href="<?php echo $this->getRootURL(); ?>about/">About</a> | 
+<a href="<?php echo $this->getRootURL(); ?>contact/">Contact</a>
