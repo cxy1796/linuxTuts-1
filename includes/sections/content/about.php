4d722586e22c585cb0a38d117d8e80296a814b1d
webstuff <webstuff@Sids-MacBook-Pro-2.local>
2014-05-13 01:59:32 -0400

    pooooooopppppp

--- includes/sections/content/about.php
+++ includes/sections/content/about.php
@@ -1,17 +1,83 @@
+<!-- by Jon! -->
 <div class="container narrow">
-	<h2>About</h2>
-	<p>
-		This is our nifty website about Unix/Linux tutorials for Web II.
-	</p>
-	<p>
-		<h3>Team Members</h3>
-		<ul>
-			<li>Sid Grabosky</li>
-			<li>Andrew Bilgore</li>
-			<li>Michael Schirmer</li>
-			<li>Cathleen Yang</li>
-			<li>Patrick Ogbeide</li>
-			<li>Jon Koch</li>
-		</ul>
-	</p>
+	<div class="container">
+		<h2>
+			About Us
+		</h2>
+		<h3>
+			The Team
+		</h3>
+		<div class="person">
+			<div class="teamMemContainer">
+				<div class="picBox">
+				</div>
+				<div class="teamMemberName">
+					Andrew Bilgore
+				</div>
+				<div class="teamMemberPosition">
+					Team Leader
+				</div>
+			</div>
+		</div>
+			<div class="person">
+				<div class="teamMemContainer">
+					<div class="picBox">
+					</div>
+					<div class="teamMemberName">
+						Sid Grabosky
+					</div>
+					<div class="teamMemberPosition">
+						Head Programmer
+					</div>
+				</div>
+			</div>
+			<div class="person">
+				<div class="teamMemContainer">
+					<div class="picBox">
+					</div>
+					<div class="teamMemberName">
+						Cathleen Yang
+					</div>
+					<div class="teamMemberPosition">
+						Artist
+					</div>
+				</div>
+			</div>
+			<div class="person">
+				<div class="teamMemContainer">
+					<div class="picBox">
+					</div>
+					<div class="teamMemberName">
+						Michael Schirmer
+					</div>
+					<div class="teamMemberPosition">
+						Content
+					</div>
+				</div>
+			</div>
+			<div class="person">
+				<div class="teamMemContainer">
+					<div class="picBox">
+					</div>
+					<div class="teamMemberName">
+						Patrick Ogbeide
+					</div>
+					<div class="teamMemberPosition">
+						Content
+					</div>
+				</div>
+			</div>
+			<div class="person">
+				<div class="teamMemContainer">
+					<div class="picBox">
+					</div>
+					<div class="teamMemberName">
+						Jon Koch
+					</div>
+					<div class="teamMemberPosition">
+						Content
+					</div>
+				</div>
+			</div>
+	</div>
 </div>
---
+++ includes/sections/css/about.php
@@ -0,0 +1,2 @@
+<?php echo $this->getSection('css', 'default'); ?>
+<link rel="stylesheet" type="text/css" href="<?php echo $this->getRootURL(); ?>/static/css/about.css" />
